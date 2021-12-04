6948e2afb9602fe4575270eb8424b6327b49e3b2
Raphael Cerqueira <raphaelfc@gmail.com>
2021-12-04 13:40:19 -0300

    Initial Commit

---
+++ MetaMaskSwiftUI/MetaMaskSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MetaMaskSwiftUIApp.swift
+//  MetaMaskSwiftUI
+//
+//  Created by Raphael Cerqueira on 04/12/21.
+//
+
+import SwiftUI
+
+@main
+struct MetaMaskSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
